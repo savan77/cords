846602d359fd064cc893b99495f6b88596e0a434
Savan Visalpara <savan77@users.noreply.github.com>
2021-04-09 13:42:42 -0500

    add config for FULL with Mnist

---
+++ configs/config_full_mnist.py
@@ -0,0 +1,41 @@
+# Learning setting
+config = dict(setting="supervisedlearning",
+
+              dataset=dict(name="mnist",
+                           datadir="../data",
+                           feature="dss",
+                           type="pre-defined"),
+
+              dataloader=dict(shuffle=True,
+                              batch_size=20,
+                              pin_memory=True),
+
+              model = dict(architecture='MnistNet',
+                    type = 'pre-defined',
+                    numclasses = 10,
+                    kernel1=3,
+                    kernel2=3),
+
+              loss=dict(type='CrossEntropyLoss',
+                        use_sigmoid=False),
+
+              optimizer=dict(type="sgd",
+                             momentum=0.9,
+                             lr=0.01,
+                             weight_decay=5e-4),
+
+              scheduler=dict(type="cosine_annealing",
+                             T_max=300),
+
+              dss_strategy=dict(type="Full",
+                                fraction=0.1,
+                                select_every=20),
+
+              train_args=dict(num_epochs=300,
+                              device="cuda",
+                              print_every=1,
+                              results_dir='results/',
+                              print_args=["val_loss", "val_acc"],
+                              return_args=[]
+                              )
+              )
