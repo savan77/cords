0eb0d0b91814b2c8a78baaf2da8bebecb8e9226f
Savan Visalpara <savan77@users.noreply.github.com>
2021-06-14 12:07:52 -0700

    remove logs

--- cords/utils/models/mnist_net.py
+++ cords/utils/models/mnist_net.py
@@ -8,20 +8,17 @@ class MnistNet(nn.Module):
     def __init__(self, kernel1=3, feat_size1 = 32, kernel2=3, feat_size2=64, drop1=0.25, drop2=0.5, fc_deep=1, fc_width=128, batch_size=20):
         super(MnistNet, self).__init__()
         self.embDim = 128
-        print("bs", batch_size)
         self.conv1 = nn.Conv2d(1, feat_size1, kernel1, 1)
         self.conv2 = nn.Conv2d(feat_size1, feat_size2, kernel2, 1)
         self.dropout1 = nn.Dropout2d(drop1)
         self.dropout2 = nn.Dropout2d(drop2)
         size = self.get_flat_fts((1,28,28), nn.Sequential(self.conv1, self.conv2))
         size = (size // 4)
-        print("sze****",size)
         self.fc1 = []
         for i in range(fc_deep):
             self.fc1.append(nn.Linear(size, fc_width))
             size = fc_width
         self.fc1 = nn.Sequential(*self.fc1)
-        print(self.fc1)
         self.fc2 = nn.Linear(fc_width, 10)
 
     def get_flat_fts(self, in_size, fts):
@@ -50,12 +47,9 @@ class MnistNet(nn.Module):
             out = F.max_pool2d(out, 2)
             out = self.dropout1(out)
             out = torch.flatten(out, 1)
-            print("out dize", out.size())
-            print("**", self.fc1)
             out = self.fc1(out)
             out = F.relu(out)
             e = self.dropout2(out)
-            print("fc2 size", e.size())
         out = self.fc2(e)
         if last:
             return out, e
